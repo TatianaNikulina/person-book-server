926ea2a66c47fd4e7afbaeb43e11b38d851750e0
Eleonora32 <eleonora.dubovia@web.de>
2021-06-28 13:44:38 +0200

    add albums

--- controllers/album.controller.js
+++ controllers/album.controller.js
@@ -1,85 +1,100 @@
 const db = require('../models');
-const User = db.users;
+const Album = db.albums;
+
+exports.create = (req, res) => {
+    Album.create({
+        ...req.body
+    })
+        .then(album => {
+            res.send(album)
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message
+            })
+        })
+}
+
 
 exports.findAll = (req, res) => {
-    User.findAll()
+    Album.findAll()
         .then(data => res.send(data))
         .catch(err => {
             res.status(500).send({
-                message: err.message || 'Some error occurred while retrieving users'
+                message: err.message || 'Some error occurred while retrieving albums'
             })
         })
 }
 
 exports.findOne = (req, res) => {
     const id = req.params.id
-    User.findByPk(id)
+    Album.findByPk(id)
         .then(data => {
             if(!data) {
                 res.status(404).send({
-                    message: `User with id=${id} not found`
+                    message: `Album with id=${id} not found`
                 })
             }
             res.send(data)
         })
         .catch(err => {
             res.status(500).send({
-                message: err.message || "Error retrieving User with id = " + id
+                message: err.message || "Error retrieving Album with id = " + id
             })
         })
 }
 
 exports.update = (req, res) => {
     const id = req.params.id
-    User.update(req.body, {
+    Album.update(req.body, {
         where: {id: id}
     })
         .then(num => {
             if (num == 1) {
-                User.findByPk(id)
+                Album.findByPk(id)
                 .then(data => {
                     if(!data) {
                         res.status(404).send({
-                            message: `User with id=${id} not found`
+                            message: `Album with id=${id} not found`
                         })
                     }
                     res.send(data)
                 })
                 .catch(err => {
                     res.status(500).send({
-                        message: err.message || "Error retrieving User with id = " + id
+                        message: err.message || "Error retrieving Album with id = " + id
                     })
                 })
             } else {
                 res.status(404).send({
-                    message: `Cannot update User info with id=${id}.`
+                    message: `Cannot update Album info with id=${id}.`
                 })
             }
         })
         .catch(err => {
             res.status(500).send({
-                message: err.message || "Error updating User info with id = " + id
+                message: err.message || "Error updating Album info with id = " + id
             })
         })
 }
 
 exports.delete= (req, res) => {
     const id = req.params.id
-    User.destroy({
+    Album.destroy({
         where: {id: id}
     })
         .then(num => {
             if (num == 1) {
-                res.send({message: "User was successfully deleted"})
+                res.send({message: "Album was successfully deleted"})
             } else {
                 res.send({
-                    message: `Cannot delete User with id=${id}.`
+                    message: `Cannot delete Album with id=${id}.`
                 })
             }
         })
         .catch(err => {
             res.status(500).send({
-                message: err.message || "Error deleting User with id = " + id
+                message: err.message || "Error deleting Album with id = " + id
             })
         })
 }
--- models/index.js
+++ models/index.js
@@ -21,5 +21,6 @@ db.Sequelize = Sequelize
 db.sequelize = sequelize
 
 db.users = require('./user.model.js')(sequelize, Sequelize)
+db.albums = require('./album.model.js')(sequelize, Sequelize)
 
 module.exports = db;
